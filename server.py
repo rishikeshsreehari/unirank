8123388eb91df1e106bce87ab1b8239785fb2078
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2024-01-25 03:28:32 +0400

    Added server code

---
+++ server.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8000 # Website will be available at http://localhost:8000/
+
+# Get the current directory (where your build.py  is located)
+current_directory = os.getcwd()
+
+# Set the current directory as the working directory for the HTTP server
+os.chdir(current_directory)
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
